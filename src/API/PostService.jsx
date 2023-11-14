d41012b2cbbfd0cf45bd07fe038df40eb6f56e49
ilya2U <102806031+ilya2U@users.noreply.github.com>
2023-11-14 23:42:58 +0300

    proverka

---
+++ src/API/PostService.jsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export default class PostService {
+    static async getAll(){
+      
+      try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            return response.data  
+            console.log('object');
+        } 
+      catch (error) {
+               console.log(error); 
+        }
+        
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import './styles/App.css';
+import PostForm from './components/PostForm';
+import PostList from './components/PostList';
+import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
+import {usePosts} from './hooks/usePosts'
+import PostService from './API/PostService';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const [posts, setPosts] = useState([]) 
+    const [modal, setModal]= useState(false)
+    const [filter,setFilter]= useState({sort:'', query:''})
+    const sortedAndSearchedPosts= usePosts(posts, filter.sort, filter.query)
+    const [isPostsLoading, setIsPostsLoading] = useState(true)
+
+   
+    useEffect(() => {
+        fetchPosts().then(()=>{
+            setIsPostsLoading(false);
+        })
+   
+    }, [])
+
+    
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+        setModal(false)
+    }
+
+    async function fetchPosts(){
+        const posts = await PostService.getAll();
+        setPosts(posts)
+    }
+
+
+    const removePost=(post)=>{
+        setPosts(posts.filter(p=>p.id!==post.id))
+        
+    }
+
+    
+
+    return (
+        <div className="App">   
+            
+            <MyButton style={{borderRight:0}} onClick={fetchPosts}>GetPosts</MyButton>
+            <MyButton style={{marginTop:30}} onClick={()=>setModal(true)}>
+                Создать пользователя
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+            <hr style={{margin:'15px 0'}} />
+            <PostFilter filter={filter} setFilter={setFilter}/>
+            {isPostsLoading
+             ? <h1 style={{textAlign: 'center'}}>Загрузка</h1>
+             :<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>}
+            
+        </div>
+    );
 }
 
 export default App;
