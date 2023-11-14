a0d3a7be273ebaaa64d3a4a9e95eecd6c6991363
ilya2U <102806031+ilya2U@users.noreply.github.com>
2023-11-15 00:03:16 +0300

    +loader

---
+++ src/components/UI/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import cl from '/Users/androsovilya/Desktop/newreact/my-app/src/components/UI/Loader/Loader.module.css';
+export default function Loader() {
+  return (
+    <div className={cl.loader}>
+        
+    </div>
+  )
+}
