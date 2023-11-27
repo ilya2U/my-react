5446a24ae800376ecceaf82bbb6168b75bcad829
ilya2U <102806031+ilya2U@users.noreply.github.com>
2023-11-27 23:38:59 +0300

    del

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -9,7 +9,7 @@ export default function Pagination({totalPages,page,changePage}) {
                  {pagesArray.map(p => <span 
                   onClick={() => changePage(p)}
                   key= {p}
-                  className={page === p ? 'page page__current' : 'page'}>{p}
+                  className={page === p ? 'page  page__current' : 'page'}>{p}
                   </span>)}
             </div>
   )
