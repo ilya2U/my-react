c27563ee87ba7dca0ca68264940c9c3093312933
ilya2U <102806031+ilya2U@users.noreply.github.com>
2023-12-01 00:15:50 +0300

    Перенправление при неверном URL

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import './styles/App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import About from './pages/About';
 import Posts from './pages/Posts';
+import Navbar from './components/UI/Navbar/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='navbar'>
-        <div className='navbar__links'>
-            <Link to="/about">О сайте</Link>
-            <Link to="/posts">Посты</Link>
-        </div>
-      </div>
+      <Navbar />
       <Routes>
         <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="*"      element={<Navigate to="/posts"/>}/>
       </Routes>
-
-      <Routes>
-        <Route path="/posts" element={<Posts/>} />
-      </Routes>
-
     </BrowserRouter>
   );
 }
