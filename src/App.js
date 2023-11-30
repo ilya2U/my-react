29ba3958be3cb993cc263595b042f39c74d4e443
ilya2U <102806031+ilya2U@users.noreply.github.com>
2023-11-30 23:26:01 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './styles/App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import About from './pages/About';
 import Posts from './pages/Posts';
 
 function App() {
   return (
     <BrowserRouter>
-
+      <div className='navbar'>
+        <div className='navbar__links'>
+            <Link to="/about">О сайте</Link>
+            <Link to="/posts">Посты</Link>
+        </div>
+      </div>
       <Routes>
         <Route path="/about" element={<About />} />
       </Routes>
