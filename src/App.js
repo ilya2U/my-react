c385d07b9bef1562ec8dd118af5a6abfe4aeb2cc
ilya2U <102806031+ilya2U@users.noreply.github.com>
2023-12-03 22:50:33 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import About from './pages/About';
-import Posts from './pages/Posts';
 import Navbar from './components/UI/Navbar/Navbar';
 import { AppRouter } from './components/AppRouter';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar/>
       <AppRouter/>
     </BrowserRouter>
   );
