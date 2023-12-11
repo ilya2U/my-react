7a5028f1708516716c50402ea1acc051d7d78849
ilya2U <102806031+ilya2U@users.noreply.github.com>
2023-12-11 23:55:59 +0300

    ы

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -5,6 +5,7 @@ import PostService from '../API/PostService';
 import Loader from '../components/UI/Loader/Loader';
 
 const PostIdPage = () => {
+
     const params= useParams();
     const [post,setPost] = useState({});
     const [comments,setComments] = useState([]);
@@ -25,7 +26,6 @@ const PostIdPage = () => {
     },[])
   return (
     <div>
-        
         <h1 style={{color:'teal', marginTop: 20}}>Пост:{params.id}</h1>
         {isLoading
             ? <Loader/>
