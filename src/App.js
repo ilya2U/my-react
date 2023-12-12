828bbddeaaad65abb70ccd0e3859a38bd57d387a
ilya2U <102806031+ilya2U@users.noreply.github.com>
2023-12-12 22:24:23 +0300

    d

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter} from 'react-router-dom';
 import Navbar from './components/UI/Navbar/Navbar';
 import { AppRouter } from './components/AppRouter';
+import { AuthContext } from './components/context';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <AppRouter/>
-    </BrowserRouter>
+    <AuthContext.Provider>
+      <BrowserRouter>
+          <Navbar/>
+          <AppRouter/>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
